a01be77fecb1cad84f9ba6249d4960ca68e8dfc7
k-ccosta <k.accosta98@gmail.com>
2025-01-02 20:16:08 -0400

    chore: projeto finalizado com todas as funcionalidades implementadas.

--- main.py
+++ main.py
@@ -1,96 +1,169 @@
 import os
 
-def carros():
-    carros = {
-        "Chevrolet Tracker": 120,
-        "Chevrolet Onix": 90,
-        "Chevrolet Spin": 150,
-        "Hyundai HB20": 85,
-        "Hyundai Tucson": 120,
-        "Fiat Uno": 60,
-        "Fiat Mobi": 70,
-        "Fiat Pulse": 130
-    }
+carros = [
+    {"nome": "Chevrolet Tracker", "valor": 120},
+    {"nome": "Chevrolet Onix", "valor": 90},
+    {"nome": "Chevrolet Spin", "valor": 150},
+    {"nome": "Hyundai HB20", "valor": 85},
+    {"nome": "Hyundai Tucson", "valor": 120},
+    {"nome": "Fiat Uno", "valor": 60},
+    {"nome": "Fiat Mobi", "valor": 70},
+    {"nome": "Fiat Pulse", "valor": 130}    
+]
+carros_reservados = []
 
-    for index, (chave, valor) in enumerate(carros.items()):
-        print(f"[{index}] - {chave} - R$ {valor}/dia")
-
-def apresentar_carros():
+def limpar_tela():
     os.system("cls" if os.name == "nt" else "clear")
 
-    print("[GARAGEM]\n")
-
-    carros()
-
+def confirmar_acao(mensagem):
     while True:
-        resposta = input("\nDeseja alugar um veículo? \n[S]/[N]: ").upper().strip()
-            
-        if resposta not in ["S", "N"]:
-            print("Erro: Por favor, insira 'S' para Sim ou 'N' para Não.")
-        else:
-            break
+        resposta = input(mensagem).upper().strip()
 
-    if resposta == "S":
-        ... #chamar função de aluguel de carro
-    else: 
-        os.system("clear") #ajustar o comando de limpeza de tela
-        iniciar_locadora()
+        if resposta in ["S", "N"]:
+            return resposta
+        
+        print("OPÇÃO INVÁLIDA! Por favor, insira 'S' para Sim ou 'N' para Não")
 
+def mostrar_carros():
+    for index, carro in enumerate(carros):
+        print(f"[{index}] - {carro['nome']} - R$ {carro['valor']}/dia")
 def alugar_carro():
-    os.system("cls" if os.name == "nt" else "clear")
-
+    limpar_tela()
     print("[Aluguel]\n")
+    mostrar_carros()
 
-    carros()   
+    reserva = []
 
     while True:
         try:
             carro_selecionado = int(input("\nQual carro deseja alugar? "))
-
-            if carro_selecionado in range(8):
-                break
+            if carro_selecionado not in range(len(carros)):
+                print("\nOPÇÃO INVÁLIDA! Por favor, insira um opção entre 0 e 7")
             else:
-                print("\nOPÇÃO INVÁLIDA! Selecione uma opção entre 0 e 7")                
+                break
         except ValueError:
-            print("\nOPÇÃO INVÁLIDA! Selecione uma opção entre 0 e 7")
+            print("\nOPÇÃO INVÁLIDA! Por favor, insira um opção entre 0 e 7")
 
+    while True:
+        try:
+            qtd_dias_de_reserva = int(input("Por quantos dias desejar alugar o carro? "))
+            break
+        except ValueError:
+            print("\nOPÇÃO INVÁLIDA. Por favor, insira um número inteiro.")
+
+    carro_selecionado = carros[carro_selecionado]
+    
+    print(f"\nVocê selecionou {carro_selecionado['nome']} por {qtd_dias_de_reserva} dias.")
+    print(f"A reserva ficou em R$ {qtd_dias_de_reserva*carro_selecionado['valor']}")
+
+    while True:
+            resposta = input("\nDeseja confirmar o aluguel?\n[S]/[N]: ").upper().strip()
+            
+            if resposta not in ["S", "N"]:
+                print("OPÇÃO INVÁLIDA! Por favor, insira 'S' para Sim ou 'N' para Não.")
+            else:
+                break
+    
+    if resposta == 'S':
+        limpar_tela()
+
+        carros_reservados.append(carro_selecionado)
+        carros.remove(carro_selecionado)
+
+        inicializar_locadora()
+
+    else:
+        limpar_tela()
+        alugar_carro()
+def devolver_carro():
+    print("[DEVOLUÇÃO]\n")
+
+    if not carros_reservados:
+        print("Nenhum carro reservado no momento.")
+
+        while True:
+            resposta = input("\nDeseja retornar ao menu principal?\n[S]/[N]: ").upper().strip()
+            
+            if resposta not in ["S", "N"]:
+                print("OPÇÃO INVÁLIDA! Por favor, insira 'S' para Sim ou 'N' para Não.")
+            else:
+                break       
+
+        if resposta == 'S':
+            limpar_tela()
+            inicializar_locadora()
+        else:
+            limpar_tela()
+            print("Obrigado por escolher a Locadora Cars by Kelvin")
+    
+    else:
+        for index, carro in enumerate(carros_reservados):
+            print(f"[{index}] - {carro['nome']} - R$ {carro['valor']}/dia")
+
+        while True:
+            try:
+                devolucao_selecionada = int(input("\nQual carro você deseja devolver? "))
+                break
+            except ValueError:
+                print("OPÇÃO INVÁLIDA! Selecione uma devolução válida")
+
+        devolucao_selecionada = carros_reservados[devolucao_selecionada]
+
+        carros.append(devolucao_selecionada)
+        carros_reservados.remove(devolucao_selecionada)
+
+        print(f"\nVocê devolveu o carro {devolucao_selecionada['nome']}. Obrigado!")
+        
+        limpar_tela()
+
+        inicializar_locadora()
 def menu_principal():
     print("-"*45)
     print("Bem-Vindo à locadora Cars by Kelvin")
     print("-"*45)
     print()
-
-def submenus():
-    opcoes_submenu = ["Mostrar Carros", "Alugar Carro", "Devolver Carro"]
-    
-    for i, opcao in enumerate(opcoes_submenu):
-        print(f"[{i}] - {opcao}")
-    
-    # while opcao_submenu_escolhida not in [0, 1, 2]:
-    #     try:
-    #         opcao_submenu_escolhida = int(input("O que você deseja fazer? "))
-    #     except ValueError:
-    #         print("\nOPÇÃO INVÁLIDA! Escolha uma opção entre 0 e 3")
+def sub_menu():
+    print()
+    print("[0] - Mostrar Carros \n[1] - Alugar Carro \n[2] - Devolver Carro")
 
     while True:
         try:
-            opcao_submenu_escolhida = int(input("\nO que você deseja fazer? "))
-            if opcao_submenu_escolhida in range(3):
-                break
+            escolha_sub_menu = int(input("\nO que deseja fazer? "))
+            if escolha_sub_menu not in range(3):
+                print("OPÇÃO INVÁLIDA! Por favor, insira um opção entre 0 e 2")
             else:
-                print("OPÇÃO INVÁLIDA! Escolha uma opção entre 0 e 2")                
+                break            
         except ValueError:
-            print("OPÇÃO INVÁLIDA! Escolha uma opção entre 0 e 2")
+            print("OPÇÃO INVÁLIDA! Por favor, insira um número inteiro.")
 
-    if opcao_submenu_escolhida == 0:
-        apresentar_carros()
-    elif opcao_submenu_escolhida == 1:
+    if escolha_sub_menu == 0:
+        limpar_tela()
+        print("[Bem-vindo à Garagem]\n")
+        mostrar_carros()
+
+        while True:
+            resposta = input("\nDeseja alugar um carro?\n[S]/[N]: ").upper().strip()
+            
+            if resposta not in ["S", "N"]:
+                print("OPÇÃO INVÁLIDA! Por favor, insira 'S' para Sim ou 'N' para Não.")
+            else:
+                break       
+
+        if resposta == 'S':
+            limpar_tela()
+            alugar_carro()
+        else:
+            limpar_tela()
+            inicializar_locadora()
+    elif escolha_sub_menu == 1:
+        limpar_tela()
         alugar_carro()
     else:
-        print("[Devolver]")
-        
-def iniciar_locadora():
-    menu_principal()
-    submenus()
+        limpar_tela()
+        devolver_carro()
 
-iniciar_locadora()
+def inicializar_locadora():
+    menu_principal()
+    sub_menu()
+
+inicializar_locadora()
